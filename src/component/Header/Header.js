c8bdeb26abd09dec4fe3e1d43830aa8ec0c1b23e
Soyeb <tssoyeb@gmail.com>
2022-03-26 18:52:49 +0600

    Header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+                <div>
+                   
+                    <a href="/shop"><b>Shop</b></a>
+                    <a href="/orders"><b>Orders</b></a>
+                    <a href="/inventory"><b>Inventory</b></a>
+                    <a href="/about"><b>About</b></a>
+                </div>
+        </nav>
+    );
+};
+
+export default Header;
